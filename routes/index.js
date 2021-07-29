1ebdfbdd253d52d3fcd83ba1ec9204bba70e1103
Sriraj Bura <sriraj.bura16@gmail.com>
2021-07-29 13:56:32 -0400

    implemeted messages view

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Mini Messageboard',
+    messages: messages,
+  });
+});
+
+router.get('/new', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
